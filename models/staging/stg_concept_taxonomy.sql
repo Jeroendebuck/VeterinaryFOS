072a676245bc94a24ae84947170ac29bd0ee9993
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-09-30 20:45:05 -0600

    Update stg_concept_taxonomy.sql

--- models/staging/stg_concept_taxonomy.sql
+++ models/staging/stg_concept_taxonomy.sql
@@ -1,10 +1,12 @@
 {{ config(materialized='view') }}
+-- Keep taxonomy fields as text for simplicity
 select
   id,
   label,
   level,
-  split(parents, ',') as parents,
-  split(coalesce(synonyms,''), '|') as synonyms,
-  split(coalesce(include_keywords,''), '|') as include_keywords,
-  split(coalesce(exclude_keywords,''), '|') as exclude_keywords
-from {{ ref('concept_taxonomy') }};
+  parents,
+  synonyms,
+  include_keywords,
+  exclude_keywords
+from {{ ref('concept_taxonomy') }}
+
