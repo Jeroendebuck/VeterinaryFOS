2f6efc65abcf5707227b4602445c85621d32fd19
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-09-30 20:44:46 -0600

    Update stg_authors.sql

--- models/staging/stg_authors.sql
+++ models/staging/stg_authors.sql
@@ -4,4 +4,5 @@ select
   cast(Name as varchar) as name,
   cast(OpenAlexID as varchar) as openalex_id,
   cast(coalesce(Affiliation, '') as varchar) as affiliation
-from read_csv_auto('data/roster_with_metrics.csv');
+from read_csv_auto('data/roster_with_metrics.csv')
+
