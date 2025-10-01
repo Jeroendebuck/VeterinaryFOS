a5cb969d537c81e1f8b26f1bb8fbd2c9c840fb6e
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-10-01 09:54:26 -0600

    Update dashboard_unit_concept_latest.sql

--- models/marts/dashboard_unit_concept_latest.sql
+++ models/marts/dashboard_unit_concept_latest.sql
@@ -3,7 +3,7 @@
 select
   m.unit_id,
   m.concept_id,
-  coalesce(c.label, m.concept_id) as concept_label,
+  coalesce(c.label, d.concept_label_openalex, m.concept_id) as concept_label,
   m.latest_period,
   m.lq,
   m.works_latest,
@@ -11,5 +11,7 @@ select
 from {{ ref('metrics_lq_growth') }} m
 left join {{ ref('stg_concept_taxonomy') }} c
   on c.id = m.concept_id
+left join {{ ref('stg_concepts_dim') }} d      -- <-- NEW
+  on d.concept_id = m.concept_id
 left join {{ ref('metrics_critical_mass') }} cm
   using (unit_id, concept_id)
