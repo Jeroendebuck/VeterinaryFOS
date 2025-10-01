3041ba2ccd6eddc29513400143e70253e56948f3
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-09-30 20:45:28 -0600

    Update stg_globals.sql

--- models/staging/stg_globals.sql
+++ models/staging/stg_globals.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='view') }}
--- Global totals per (concept_id, year or period) for LQ denominators
+-- Global totals per (concept_id, period) for LQ denominators
 -- columns: period, concept_id, global_works
-select * from read_csv_auto('data/globals_concepts.csv');
+select * from read_csv_auto('data/globals_concepts.csv')
