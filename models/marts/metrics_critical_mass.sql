1874e67f71907d4cfa32bbb94551585635c3e2ac
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-09-30 20:58:26 -0600

    Update metrics_critical_mass.sql

--- models/marts/metrics_critical_mass.sql
+++ models/marts/metrics_critical_mass.sql
@@ -1,12 +1,13 @@
 {{ config(materialized='view') }}
+
 with base as (
   select *,
-         coalesce(active_author_hits,0) as active_hits
+         coalesce(active_author_hits, 0) as active_hits
   from {{ ref('int_unit_concept_period') }}
 ),
 ranked as (
   select *,
-    row_number() over(partition by unit_id, concept_id order by period desc) as rn
+         row_number() over (partition by unit_id, concept_id order by period desc) as rn
   from base
 )
 select
@@ -16,4 +17,4 @@ select
   active_hits,
   (active_hits >= {{ var('critical_mass_threshold') }}) as has_critical_mass
 from ranked
-where rn = 1;
+where rn = 1
