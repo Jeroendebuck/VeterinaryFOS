b93eb79a3c915361c48a5a3a056bd796cd6eb886
Jeroendebuck <51967425+Jeroendebuck@users.noreply.github.com>
2025-09-30 21:07:13 -0600

    Update stg_authors.sql

--- models/staging/stg_authors.sql
+++ models/staging/stg_authors.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 -- Expecting data/roster_with_metrics.csv: columns Name, OpenAlexID, Affiliation
 select
-  cast(r."Name" as varchar)        as name,
-  cast(r."OpenAlexID" as varchar)  as openalex_id,
-  cast(coalesce(r."Affiliation", '')) as affiliation
+  cast(r."Name" as varchar)       as name,
+  cast(r."OpenAlexID" as varchar) as openalex_id,
+  cast(coalesce(r."Affiliation", '') as varchar) as affiliation
 from read_csv_auto('data/roster_with_metrics.csv') as r
